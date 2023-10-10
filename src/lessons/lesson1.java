a8376ce6b69ddf30bd6b4cf4af8dff8ab299a3c8
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-10 21:37:33 +0300

    Initial commit

---
+++ src/lessons/lesson1.java
@@ -0,0 +1,13 @@
+package lessons;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class lesson1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        b = a;
+        System.out.println(a+2);
+        System.out.println(b);
+    }
+}
