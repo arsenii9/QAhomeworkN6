d45ea29eb077cc45db65c84dd5901416b48a6173
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-14 09:16:53 +0300

    hw to lesson 2

---
+++ src/lessons/lesson2hw.java
@@ -0,0 +1,12 @@
+package lessons;
+
+public class lesson2hw {    public static void main(String[] args) {
+    int a = 7;
+    int b = 9;
+    b = a;
+    System.out.println(a+2);
+    System.out.println(b);
+}
+}
+
+
