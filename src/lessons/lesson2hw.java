049283f538456fc2724d70c47eeb46bcb32dbc49
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-18 19:01:35 +0300

    hw to lesson 3

--- src/lessons/lesson2hw.java
+++ src/lessons/lesson2hw.java
@@ -1,15 +1,21 @@
 package lessons;
 
-public class lesson2hw {    public static void main(String[] args) {
+public class lesson2hw {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
 
-    int a = 7;
-    int b = 9;
-    a = a + b%a;
-    b = ++a;
-    b = ++a;
-    System.out.println(a);
-    System.out.println(b);
+        System.out.println("До обмена:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Обмен значений a и b
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("После обмена:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
 }
-}
-
-
