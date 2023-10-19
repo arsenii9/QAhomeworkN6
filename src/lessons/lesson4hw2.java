f082601b84e400a1025a51e27cda2484d81c7c19
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-19 10:08:13 +0300

    hw to lesson 3

---
+++ src/lessons/lesson4hw2.java
@@ -0,0 +1,28 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class lesson4hw2 {
+    public static void main(String[] args) {
+        System.out.println("ВВедите два числа чтобы получить их разницу.");
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = x - y;
+
+        if (z > 0){
+        System.out.println(z);}
+        else if (z < 0){
+         System.out.println(-z);
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
