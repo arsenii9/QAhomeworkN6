bec0bdfd4483f9b69718de4a2a514ff27f354758
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-20 13:24:57 +0300

    hw to lesson 3

---
+++ src/lessons/lesson4hw6.java
@@ -0,0 +1,50 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class lesson4hw6 {
+    public static void main(String[] args) {
+        System.out.println("Просим вас вводить все названия с большой буквы!");
+        System.out.println("Введите выбранную прогармму.");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        switch (string){
+            case ("Java"):
+                System.out.println("Введите выбранную операционную систему.");
+                Scanner scanner1 = new Scanner(System.in);
+                String string1 = scanner1.nextLine();
+                if (string1.equals("Windows")){
+                    System.out.println("https://www.java.com/ru/download/manual.jsp");}
+               else if (string1.equals("Macos")){
+                    System.out.println("https://www.java.com/ru/download/manual.jsp");}
+               else if (string1.equals("Linux")){
+                    System.out.println("https://www.java.com/ru/download/manual.jsp");}
+            case ("Git"):
+                System.out.println("Введите выбранную операционную систему.");
+                Scanner scanner2 = new Scanner(System.in);
+                String string2 = scanner2.nextLine();
+                if (string2.equals("Windows")){
+                    System.out.println("https://gitforwindows.org/");}
+               else if (string2.equals("Macos")){
+                    System.out.println("https://git-scm.com/download/mac");}
+               else if (string2.equals("Linux")){
+                    System.out.println("https://git-scm.com/download/linux");}
+            case ("IntelliJ idea"):
+                System.out.println("Введите выбранную операционную систему.");
+                Scanner scanner3 = new Scanner(System.in);
+                String string3 = scanner3.nextLine();
+                if (string3.equals("Windows")){
+                    System.out.println("https://www.jetbrains.com/idea/download/?section=windows");}
+               else if (string3.equals("Macos")){
+                   System.out.println("https://www.jetbrains.com/idea/download/?section=mac");}
+               else if (string3.equals("Linux")){
+                    System.out.println("https://www.jetbrains.com/idea/download/?section=linux");}
+            default:
+                System.out.println("Была допущена ошибка при введении названия программы или операционной системы");
+
+
+
+
+        }
+    }
+}
---
+++ src/lessons/lesson5.java
@@ -0,0 +1,25 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class lesson5 {
+    public static void main(String[] args) {
+       /* 4)Пользователь вводит слово с клавиатуры, оно выводится в консоль таким образом, что каждый символ
+        выводится через пробел. Используем для этого цикл for.
+        Пример: пользователь ввел слово "Картина", в консоль выводится "К а р т и н а"
+    }*/
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        int integer = string.length();
+        for (int i = 0 ; i < integer ; i++){
+
+            System.out.print(string.charAt(i) +  " ");
+
+
+
+        }
+    }
+
+        }
+
+
