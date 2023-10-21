bb4de588ce4ce8322d2f09c3e91e7d697e206453
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-21 09:44:19 +0300

    hw to lesson 3

---
+++ src/lessons/lesson5hw5.java
@@ -0,0 +1,29 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class lesson5hw5 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 4, 7, 10, 15, 23, 45, 60, 77, 99};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число для поиска: ");
+        int searchNumber = scanner.nextInt();
+
+        boolean found = false;
+        for (int number : numbers) {
+            if (number == searchNumber) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Число найдено в массиве.");
+        } else {
+            System.out.println("Число не найдено в массиве.");
+
+    }
+}
+}
+
