3328a18148f22331f1a188eecfa4ffd44fc83638
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-21 09:39:31 +0300

    hw to lesson 3

---
+++ src/lessons/lesson5hw3.java
@@ -0,0 +1,28 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class lesson5hw3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int maxCapacity = 100; // Максимальная вместимость склада
+        int currentCapacity = maxCapacity;
+
+        while (currentCapacity > 0) {
+            System.out.println("Склад может принять " + currentCapacity + " кг металла.");
+            System.out.print("Введите вес металла для приема: ");
+            int weight = scanner.nextInt();
+
+            if (weight < 5) {
+                System.out.println("Невозможно принять металл весом менее 5 кг.");
+            } else if (weight > currentCapacity) {
+                System.out.println("Недостаточно места на складе для приема этого металла.");
+            } else {
+                currentCapacity -= weight;
+                System.out.println("Металл принят. Осталось места на складе: " + currentCapacity + " кг.");
+            }
+        }
+
+        System.out.println("Склад заполнен. Программа завершена.");
+    }
+}
