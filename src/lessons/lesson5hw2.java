74e4242b9d3f3691c39a2472727a27303529e47f
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-21 09:36:33 +0300

    hw to lesson 3

---
+++ src/lessons/lesson5hw2.java
@@ -0,0 +1,23 @@
+package lessons;
+
+import java.util.Scanner;
+
+public class lesson5hw2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Это число является палиндромом.");
+        } else {
+            System.out.println("Это число не является палиндромом.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+}
