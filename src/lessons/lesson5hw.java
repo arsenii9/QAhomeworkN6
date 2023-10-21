fe5839529b75f00c72978e847e3c6cb01ada74f1
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-21 08:06:15 +0300

    hw to lesson 3

--- src/lessons/lesson5hw.java
+++ src/lessons/lesson5hw.java
@@ -1,7 +1,33 @@
 package lessons;
 
+import java.util.Scanner;
+
 public class lesson5hw {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = new String[100]; // Максимальное количество слов, которое может быть в предложении
+        int wordCount = 0;
 
+        System.out.println("Введите слова (для завершения введите 'STOP'):");
+
+        while (true) {
+            String word = scanner.next();
+
+            if (word.equalsIgnoreCase("STOP")) {
+                break;
+            }
+
+            words[wordCount] = word;
+            wordCount++;
+        }
+
+        System.out.print("Ваше предложение: ");
+
+        for (int i = 0; i < wordCount; i++) {
+            System.out.print(words[i] + " ");
+        }
+
+        scanner.close();
     }
+
 }
