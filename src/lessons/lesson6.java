f27985fcda78059f6801a8709742ea54996f0bb1
arsenii9 <antonenko.arsenii@gmail.com>
2023-10-23 09:15:21 +0300

    hw to lesson 3

---
+++ src/lessons/lesson6.java
@@ -0,0 +1,29 @@
+package lessons;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class lesson6 {
+    /*Заполнить массив на 10 элементов случайными числами от 0 до 100.
+    Вывести в консоль все значения данного массива.
+    Найти минимальный элемент и вывести его на консоль.*/
+    public static void main(String[] args) {
+        int[] integer = new int[10];
+        Random random = new Random();
+
+       for (int i = 0; i < integer.length;i++){
+integer[i] = random.nextInt(100);
+       }
+        System.out.println(Arrays.toString(integer));
+        int min = integer[0];
+        for (int i = 0; i < integer.length;i++){
+            boolean b = min<integer[i];
+            if (b == false ){
+                min = integer[i];
+
+            }
+        }
+        System.out.println(min);
+
+    }
+}
---
+++ src/lessons/lesson6hw1.java
@@ -0,0 +1,44 @@
+package lessons;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class lesson6hw1 {
+    public static void main(String[] args) {
+        Random random = new Random( );
+        int random2 = random.nextInt(101);
+        String[] abc = new String[2];
+        abc [0] = "Tell your name please" ;
+        abc [1] = "Let the game begin!";
+        System.out.println(abc[0]);
+        Scanner n = new Scanner(System.in);
+        String name = n.nextLine();
+        System.out.println(abc[1]);
+        while (true){
+            String[] abcd = new String[2];
+            abcd [0] = "Your number is too small. Please, try again...";
+            abcd [1] =  "Your number is too big. Please, try again...";
+            Scanner scanner = new Scanner(System.in);
+            int answer = scanner.nextInt();
+            if (random2 > answer){
+                System.out.println(abcd[0]);
+            }
+            else if (random2 < answer){
+                System.out.println(abcd[1]);
+            }
+            else {
+                System.out.println("Congratulations, " + name + "!");
+            }
+
+
+            }
+
+
+
+
+
+
+}
+
+    }
+
