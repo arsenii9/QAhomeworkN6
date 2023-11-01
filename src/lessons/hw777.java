e35697787ccb688ab7ede25862ea29dad9cf934f
arsenii9 <antonenko.arsenii@gmail.com>
2023-11-01 17:11:45 +0200

    hw to lesson 3

--- src/lessons/hw777.java
+++ src/lessons/hw777.java
@@ -1,13 +1,22 @@
 package lessons;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class hw777 {
     public static void main(String[] args) {
         char[][] gameBoard = new char[5][5];
-        initializeGameBoard(gameBoard);
-        placeTarget(gameBoard);
+
+        // Initialize the game board
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                gameBoard[i][j] = '-';
+            }
+        }
+
+        Random random = new Random();
+        int targetRow = random.nextInt(5);
+        int targetCol = random.nextInt(5);
+        gameBoard[targetRow][targetCol] = 'x';
 
         System.out.println("All Set. Get ready to rumble!");
 
@@ -15,9 +24,35 @@ public class hw777 {
         boolean gameWon = false;
 
         while (!gameWon) {
-            printGameBoard(gameBoard);
-            int row = getValidInput("Enter row (1-5): ", 1, 5) - 1;
-            int col = getValidInput("Enter column (1-5): ", 1, 5) - 1;
+            System.out.println("  1 2 3 4 5");
+            for (int i = 0; i < gameBoard.length; i++) {
+                System.out.print(i + 1);
+                for (int j = 0; j < gameBoard[i].length; j++) {
+                    System.out.print(" ");
+                    System.out.print(gameBoard[i][j]);
+                }
+                System.out.println();
+            }
+
+            int row;
+            int col;
+
+            System.out.print("Enter row (1-5): ");
+            while (!scanner.hasNextInt() || (row = scanner.nextInt()) < 1 || row > 5) {
+                System.out.println("Invalid input. Please enter a number between 1 and 5.");
+                scanner.nextLine();
+                System.out.print("Enter row (1-5): ");
+            }
+
+            System.out.print("Enter column (1-5): ");
+            while (!scanner.hasNextInt() || (col = scanner.nextInt()) < 1 || col > 5) {
+                System.out.println("Invalid input. Please enter a number between 1 and 5.");
+                scanner.nextLine();
+                System.out.print("Enter column (1-5): ");
+            }
+
+            row -= 1;
+            col -= 1;
 
             if (gameBoard[row][col] == 'x') {
                 gameWon = true;
@@ -29,47 +64,4 @@ public class hw777 {
         }
         scanner.close();
     }
-
-    private static void initializeGameBoard(char[][] gameBoard) {
-        for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j < gameBoard[i].length; j++) {
-                gameBoard[i][j] = '-';
-            }
-        }
-    }
-
-    private static void placeTarget(char[][] gameBoard) {
-        Random random = new Random();
-        int targetRow = random.nextInt(5);
-        int targetCol = random.nextInt(5);
-        gameBoard[targetRow][targetCol] = 'x';
-    }
-
-    private static void printGameBoard(char[][] gameBoard) {
-        System.out.println("  1 2 3 4 5");
-        for (int i = 0; i < gameBoard.length; i++) {
-            System.out.print(i + 1);
-            for (int j = 0; j < gameBoard[i].length; j++) {
-                System.out.print(" ");
-                System.out.print(gameBoard[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    private static int getValidInput(String prompt, int min, int max) {
-        int input;
-        Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.print(prompt);
-            while (!scanner.hasNextInt()) {
-                System.out.println("Invalid input. Please enter a number between " + min + " and " + max + ".");
-                scanner.next();
-                System.out.print(prompt);
-            }
-            input = scanner.nextInt();
-        } while (input < min || input > max);
-        return input;
-    }
 }
-
