323010ac460b137ea0db1743dc6578f3de3da93d
arsenii9 <antonenko.arsenii@gmail.com>
2023-11-04 10:00:32 +0200

    hw to lesson 3

---
+++ src/lessons/Lesson8hw3.java
@@ -0,0 +1,10 @@
+package lessons;
+
+public class Lesson8hw3 {
+    public String studentFullName;
+    public String itemName;
+    public String coloredCovers;
+    public String heNameOfTheInstitutionWhereTheStudentIsStudying;
+    public int yearOfIssue;
+    public int numberOfPages;
+}
